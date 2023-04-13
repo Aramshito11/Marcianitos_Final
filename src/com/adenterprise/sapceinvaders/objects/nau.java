4edeff653bd8cffd103a5c2d28e2d5e8b0a2ad70
danigelabert <73782173+danigelabert@users.noreply.github.com>
2023-04-13 13:53:52 +0200

    NAUS CREADES

--- src/com/adenterprise/sapceinvaders/objects/nau.java
+++ src/com/adenterprise/sapceinvaders/objects/nau.java
@@ -1,7 +1,38 @@
 package com.adenterprise.sapceinvaders.objects;//Aram
 
 public class nau {
-    public static void main(String[] args) {
+    private int posicio;
+    private int vides;
 
+    public nau(int posicio, int vides) {
+        this.posicio = posicio;
+        this.vides = vides;
+    }
+
+    public int getPosicio() {
+        return posicio;
+    }
+
+    public void setPosicio(int posicio) {
+        this.posicio = posicio;
+    }
+
+    public int getVides() {
+        return vides;
+    }
+
+    public void setVides(int vides) {
+        this.vides = vides;
+    }
+
+    public void showData(){
+        System.out.println("\n\nNAU");
+        System.out.println("Posicio: "+getPosicio());
+        System.out.println("Vides: "+getVides());
+    }
+
+    public static void main(String[] args) {
+        nau n1=new nau(1, 3);
+        n1.showData();
     }
 }
